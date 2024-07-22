6f63b03163d483eed1a1f81908ec6ddf1ff587c1
sinthushan <sinthushan@gmail.com>
2024-07-22 19:44:04 -0400

    finalizing constructur for filesystem class and adding PWD and ls commads

--- src/app/terminal/prompt/folders/folders.models.ts
+++ src/app/terminal/prompt/folders/folders.models.ts
@@ -5,16 +5,22 @@ interface File {
 
 interface Folder{
     name: string;
-    folders: Folder[]
+    folders?: Folder[]
     parent?: Folder
-    files: File[];
+    files?: File[];
 }
 
+interface FolderList{
+    type: string;
+    name: string;
+}
 
 export class FileSystem{
     current_directory: Folder
-    constructor(public folders: Folder[], public files: File[]){
-        this.current_directory = folders[0]
+    home_folder: Folder
+    constructor(public folder_schema: Folder){
+        this.home_folder = this.makeFromSchema(folder_schema)
+        this.current_directory = this.home_folder
     }
     
     change_directory(path: string): Folder {
@@ -28,8 +34,10 @@ export class FileSystem{
                     this.current_directory = starting_directory
                     throw new Error(`you have reached the root directory`)
                 }
+            }else if (folder == '~'){
+                this.current_directory = this.home_folder
             }else{
-                let matching_folders =  this.current_directory.folders.filter(subfolder => subfolder.name == folder)
+                let matching_folders =  this.current_directory.folders?.filter(subfolder => subfolder.name == folder)
                 if (matching_folders === undefined || matching_folders.length == 0) {
                     this.current_directory = starting_directory
                     throw new Error(`There is no directory : ${folder} found in directory: ${this.current_directory}`)
@@ -40,4 +48,82 @@ export class FileSystem{
         })
         return this.current_directory
     }
+
+    presentWorkingDirectory(): string{
+        let path_folder = this.current_directory
+        let path_str = ""
+        while (path_folder.parent){
+            path_str = path_folder.name + '/' + path_str
+            path_folder = path_folder.parent
+        }
+        return path_folder.name + '/' + path_str
+    }
+    list(): FolderList[]{
+        let folder_list: FolderList[] = []
+        if (this.current_directory.folders){
+            this.current_directory.folders.forEach(folder => folder_list.push({
+                type: "folder",
+                name: folder.name
+            }))
+        }
+        if (this.current_directory.files){
+            this.current_directory.files.forEach(file => folder_list.push({
+                type: "file",
+                name: file.name
+            }))
+        }
+        return folder_list
+    } 
+
+    makeFromSchema(folder_schema: Folder, parent?: Folder) : Folder{
+        if (folder_schema.folders){
+            folder_schema.folders.forEach(folder => {this.makeFromSchema(folder, folder_schema)})
+        }
+        if (parent !== null){
+            folder_schema.parent = parent
+            return folder_schema
+        } else{
+            return folder_schema
+        }
+       
+    }
+
+
 }
+
+// let home_folder = {
+//     name: 'home',
+//     folders: [{
+//         name: "PROJECTS",
+//         folders:[
+//             {
+//                 name: 'secret1',
+//                 files: [{
+//                     name: 'secret1.py',
+//                     href: 'projects/secret1/secret1.py'
+//                 }]
+//             },
+//             {
+//                 name: 'secret2',
+//                 files: [{
+//                     name: 'secret2.py',
+//                     href: 'projects/secret1/secret2.py'
+//                 }]
+//             }
+//         ],
+//         files: [{
+//             name: 'Project1',
+//             href: '/projects/project1'
+//         }]
+//     }],
+// }
+
+
+// let folder = new FileSystem(home_folder)
+// folder.change_directory("PROJECTS/secret2")
+// console.log(folder.presentWorkingDirectory())
+// console.log(folder.list())
+// console.log(folder.current_directory.parent?.name)
+// console.log(folder.current_directory.parent?.parent?.name)
+// folder.change_directory("../..")
+// console.log(folder.list())
